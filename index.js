92b14138ac81015681ba576f007199baea8fd13f
agger89 <agger2154@gmail.com>
2019-04-21 22:35:55 +0900

    route test

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`)
 }
 
+function handleHome(req, res) {
+    res.send("hihihi");
+}
+
+function handleProfile(req, res) {
+    res.send("hohohho");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
